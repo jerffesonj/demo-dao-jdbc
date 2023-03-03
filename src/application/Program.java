77bdb922a1af9e6cd8aa4e6046a3c33537ab011b
Black Paladins <jerffesonj@gmail.com>
2023-03-03 11:24:49 -0300

    Find by Id implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department departmentObj = new Department(1, "Books");
-
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, departmentObj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println(departmentObj);
+		Seller seller = sellerDao.findById(7);
 		
-		System.out.println(seller);
+		System.out.println(seller.toString());
 	}
 
 }
